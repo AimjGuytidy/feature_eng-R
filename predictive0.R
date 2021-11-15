466b75e0a93465e4a6271b035d3ad81d90a3c3d0
Aimj <user@DESKTOP-17V2CUE>
2021-11-15 11:36:55 +0200

    commit

--- predictive0.R
+++ predictive0.R
@@ -212,3 +212,30 @@ summary(modelDifferences)
 
 #REGRESSION MODELS
 #####################################################
+
+#Variance-bias trade off
+
+observed <- c(0.22, 0.83, -0.12, 0.89, -0.23, -1.30, -0.15, -1.4,0.62, 0.99, 
+              -0.18, 0.32, 0.34, -0.30, 0.04, -0.87, 0.55, -1.30, -1.15, 0.20)
+predicted <- c(0.24, 0.78, -0.66, 0.53, 0.70, -0.75, -0.41, -0.43,
+               0.49, 0.79, -1.19, 0.06, 0.75, -0.07, 0.43, -0.42,
+               -0.25, -0.64, -1.26, -0.07)
+residualValues <- observed - predicted
+summary(residualValues)
+#let's visualize the relationships
+extRange <- extendrange(c(observed,predicted))
+par(mfrow=c(1,2))
+plot(observed,predicted,
+     ylim = extRange,
+     xlim = extRange)
+abline(0,1,col="red",lty=2)
+plot(predicted,residualValues,
+     ylab = "residuals")
+abline(h=0,col="blue",lty=2)
+
+#let's calculate the r2 and different correlations
+require(caret)
+R2(predicted,observed)
+cor(predicted,observed)
+RMSE(predicted,observed)
+cor(predicted,observed,method = "spearman") #rank correlation
